ced387bb0dcaa9889fa2e8289f13efe5454d88f5
Achinda <achindamihiruk@gmail.com>
2023-08-06 22:22:34 +0530

    Exercises

---
+++ Demo18.java
@@ -0,0 +1,13 @@
+public class Demo18 {
+    public static void main(String[] args) {
+        byte a = -128;
+        int b = ~a >>> 28;
+        int c = b * 2 + ++a;
+        System.out.println(c);
+
+        byte myByte = ~0;
+        int myInt = myByte >> (31 - myByte);
+        myInt = myInt << 1;
+        System.out.println(myInt);
+    }
+}
